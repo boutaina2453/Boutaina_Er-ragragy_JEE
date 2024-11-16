969bd1602f03752b0d7683b6bca26bf7a3c896d4
boutaina2453 <sr875949@gmail.com>
2024-11-16 15:55:01 +0100

    Tp4 Boutaina Erragragy

---
+++ univerity/src/main/java/ma/enset/univerity/UniverityApplication.java
@@ -0,0 +1,14 @@
+package ma.enset.univerity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class .0
+UniverityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniverityApplication.class, args);
+    }
+
+}
---
+++ univerity/src/test/java/ma/enset/univerity/UniverityApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.univerity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniverityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
